e5b2609e8a40d8c06578c2a8f54a901970d392a3
Mark <157068659+markmcla74@users.noreply.github.com>
2026-06-06 15:03:05 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'slideswap-v4'; // Bumped version to force phone cache update
+const CACHE_NAME = 'slideswap-v5'; // Bumped version to force phone cache update
 const ASSETS = [
   './',
 './index.html',
